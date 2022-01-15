31ba8af23f3376f20bc0b8d7d30c116bd03e18bd
JuanChandoqui <193242@ids.upchiapas.edu.mx>
2022-01-14 20:43:05 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#import libraries PyQt5
+#import the name of the file from designer 
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5 import uic 
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('./Views/main.ui', self) 
+    
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Window()
+    demo.show()
+
+    try: 
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window...')
