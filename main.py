0d2def606cb155903e90283ab2a0f6642bda2a8c
JuanChandoqui <193242@ids.upchiapas.edu.mx>
2022-01-20 11:32:41 -0600

    new interface with parameters

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-#import libraries PyQt5
-#import the name of the file from designer 
-import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5 import uic 
-
 
 class Window(QMainWindow):
     def __init__(self):
@@ -13,6 +9,9 @@ class Window(QMainWindow):
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    
+
     demo = Window()
     demo.show()
 
